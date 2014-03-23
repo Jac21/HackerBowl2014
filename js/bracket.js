6f10c9dfa0efcacf32f2cbc3a060c33096919082
Devin Carr <devin.carr0@gmail.com>
2014-03-22 23:19:47 -0500

    Minor fixes and corrections

--- js/bracket.js
+++ js/bracket.js
@@ -1,7 +1,5 @@
-var Mustache = require('mustache'),
-	fs = require('fs'),
-	jsdom = require('jsdom'),
-	parse = require('./js/parse.js');
+var fs = require('fs'),
+	parse = require('./parse.js');
 
 module.exports = {
 	build: function(response) {
@@ -10,12 +8,13 @@ module.exports = {
 		// send parse.build(obj, templateHtml, templateID, html, divID, response)
 
 		parse.build({"round": 1, "names": "name!"}, "/Template/template-brackets.html", "template-round", "/index.html", "bracket", response);
-		parseData: function() {
-			this.parsed['users'] = this.data['rounds'][round];
-			this.parsed['rounds'] = round;
-			for (var a in this.parsed['users']) {
-				a = '<div class="col-md-6 col-xs-12">' + a + '</div>';
-			}
+		
+	},
+	parseData: function() {
+		this.parsed['users'] = this.data['rounds'][round];
+		this.parsed['rounds'] = round;
+		for (var a in this.parsed['users']) {
+			a = '<div class="col-md-6 col-xs-12">' + a + '</div>';
 		}
 	}
 }
--- server.js
+++ server.js
@@ -4,16 +4,19 @@ var bracket = require('./js/bracket.js');
 var menu = require('./js/menu.js');
 
 http.createServer(function (req, res) {
-	if (req.url.indexOf('bracket', req.url.length-1) !== -1) {     // bracket
-        res.writeHead(200, {'Content-Type': 'text/html'});
-            console.log('Client called: ' + req.url);
-            bracket.build(req, res);
-    } else if (req.url === '/index.html' || req.url === '/') {     // index.html
+	if (req.url.indexOf('bracket', req.url) !== -1) {     // bracket
         res.writeHead(200, {'Content-Type': 'text/html'});
         console.log('Client called: ' + req.url);
-        res.write(html);
-        res.close();
-    } else if (req.url.indexOf('template', req.url.length-1) !== -1) {      // templates
+        bracket.build(req, res);
+    } else if (req.url === '/index.html' || req.url === '/') {     // index.html
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('./index.html', function(err, html) {
+            console.log('Client called: ' + req.url);
+            if (err) throw err;
+            res.write(html);
+            res.end();
+        });
+    } else if (req.url.indexOf('template', req.url) !== -1) {      // templates
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.readFile('.'+req.url, function(err, template) {
             console.log('Client called: ' + req.url);
@@ -21,7 +24,7 @@ http.createServer(function (req, res) {
             res.write(template);
             res.end();
         });
-    } else if (req.url.indexOf('.js', req.url.length-1) !== -1) {
+    } else if (req.url.indexOf('js', req.url) !== -1) {       // js
         res.writeHead(200, {'Content-Type': 'text/plain'});
         fs.readFile('.'+req.url, function(err, data) {
             console.log('Client called: ' + req.url);
@@ -29,7 +32,7 @@ http.createServer(function (req, res) {
             res.write(data);
             res.end();
         });
-    } else if (req.url.indexOf('.css', req.url.length-1) !== -1) {       // css
+    } else if (req.url.indexOf('/css/', req.url) !== -1) {       // css
         res.writeHead(200, {'Content-Type': 'text/css'});
         fs.readFile('.'+req.url, function(err, data) {
             console.log('Client called: ' + req.url);
@@ -37,7 +40,7 @@ http.createServer(function (req, res) {
             res.write(data);
             res.end();
         });
-    } else if (req.url.indexOf('json', req.url.length-1) !== -1) {      // json
+    } else if (req.url.indexOf('.json', req.url) !== -1) {      // json
         res.writeHead(200, {'Content-Type': 'text/json'});
         console.log('Client called: ' + req.url);
         fs.readFile('.'+req.url, function(err, data) {
@@ -45,10 +48,12 @@ http.createServer(function (req, res) {
             res.write(data);
             res.end();
         });
-    } else if (req.url,indexOf('Menu.html', req.url.length-1) !== -1) {
+    } else if (req.url.indexOf('Menu.html', req.url) !== -1) {     // menu.html
         res.writeHead(200, {'Content-Type': 'text/html'});
         console.log('Client called: ' + req.url);
         menu.build(req, res);
+    } else {
+        console.log('Failed call: ' + req.url);
     }
 }).listen(80);
 
