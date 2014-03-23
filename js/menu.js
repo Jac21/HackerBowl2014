9cdb098a1c47a2ca3bf24c9d33c6e48e6dbebf3c
bote795 <nbotell2@gmail.com>
2014-03-23 00:38:16 -0500

    added walker through files

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,69 @@
 var fs = require('fs'),
 	parse = require('./parse.js');
-
+	myfiles = [];
+	walkPath = './../Tournaments/';
 module.exports = {
-	build: function(response) {
-		// open correct json
+	build: function(response) 
+	{
+		var walk = function (dir, done) 
+		{
+		    fs.readdir(dir, function (error, list) 
+		    {
+		        if (error) 
+		        {
+		            return done(error);
+		        }
+		 
+		        var i = 0;
+		 
+		        (function next () 
+		        {
+		            var file = list[i++];
+		 
+		            if (!file) 
+		            {
+		                return done(null);
+		            }
+		            
+		            file = dir + '/' + file;
+		            
+		            fs.stat(file, function (error, stat) 
+		            {
+		        
+		                if (stat && stat.isDirectory()) 
+		                {
+		                    walk(file, function (error) 
+		                    {
+		                        next();
+		                    });
+		                } else {
+		                    // do stuff to file here
+		                    console.log(file);
+		                    next();
+		                }
+		            });
+		        })();
+		    });
+		};
+		 
+		 
+		console.log('-------------------------------------------------------------');
+		console.log('processing...');
+		console.log('-------------------------------------------------------------');
+		 
+		walk(walkPath, function(error) 
+		{
+		    if (error) 
+		    {
+		        throw error;
+		    } 
+		    else 
+		    {
+		        console.log('-------------------------------------------------------------');
+		        console.log('finished.');
+		        console.log('-------------------------------------------------------------');
+		    }
+		});
 		// parse json
 		// send parse.build(obj, templateHtml, templateID, html, divID, response)
 
@@ -15,6 +75,6 @@ module.exports = {
 		var type =this.parsed['users'];
 		
 	},
-	obj: {},
+	obj: [],
 	parsed: {}
 }
